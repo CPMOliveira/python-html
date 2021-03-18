a99214cb33cbab4a8cda343729e69bef99e9dbd6
Cristiano Oliveira <cristiano@labsid.eng.br>
2021-03-18 22:23:48 +0000

    DB

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-from app import routes
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+from app import routes, models
+
+db = SQLAlchemy(app)
